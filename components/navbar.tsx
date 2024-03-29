b284c95b8cd50de76aa736500e34c4b4f8185445
Arihant Jain <jainarihant.aj20@gmail.com>
2024-03-29 22:44:57 +0530

    siging fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,7 @@ export const  Navbar = ({
             </Button>
           )}
           <ModeToggle />
-          <UserButton />
+          <UserButton afterSignOutUrl="/"/>
         </div>
       </div>
     );
